d407dc46a855bf795dbfda21fc149fdbc66c9bac
Shuang Chen <shuang.chen@momenta.ai>
2025-03-27 23:41:33 +0800

    parse options

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include "spdlog/sinks/basic_file_sink.h"
 #include <clang/AST/ASTContext.h>
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/FrontendActions.h>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
+#include <spdlog/spdlog.h>
 
 using namespace clang;
 using namespace clang::tooling;
@@ -48,18 +50,32 @@ public:
 };
 
 // Command line options
-static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+static llvm::cl::OptionCategory MyToolCategory("SastDog options");
 
 int main(int argc, const char **argv) {
-  auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
-  if (!ExpectedParser) {
-    llvm::errs() << ExpectedParser.takeError();
-    return 1;
-  }
-  CommonOptionsParser &OptionsParser = *ExpectedParser;
+  llvm::cl::HideUnrelatedOptions(MyToolCategory);
 
-  ClangTool Tool(OptionsParser.getCompilations(),
-                 OptionsParser.getSourcePathList());
+  // 定义一个命令行选项，并将其归类到自定义类别中
+  static llvm::cl::opt<std::string> compileCommandFilePath(
+      llvm::cl::Positional, llvm::cl::desc("path to compile_command.json"),
+      llvm::cl::Required, llvm::cl::cat(MyToolCategory));
 
-  return Tool.run(newFrontendActionFactory<FunctionFrontendAction>().get());
+  llvm::cl::ParseCommandLineOptions(argc, argv, "SastDog\n");
+
+  spdlog::info("Path to compiler command file: " + compileCommandFilePath);
+
+  // 设置日志级别为 info
+  spdlog::set_level(spdlog::level::info);
+
+  // auto ExpectedParser = CommonOptionsParser::create(argc, argv,
+  // MyToolCategory); if (!ExpectedParser) {
+  //   llvm::errs() << ExpectedParser.takeError();
+  //   return 1;
+  // }
+  // CommonOptionsParser &OptionsParser = *ExpectedParser;
+
+  // ClangTool Tool(OptionsParser.getCompilations(),
+  //                OptionsParser.getSourcePathList());
+
+  // return Tool.run(newFrontendActionFactory<FunctionFrontendAction>().get());
 }
