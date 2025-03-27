8e806d17dc6caefd95bd47266f5a3b24c82ec1df
Shuang Chen <shuang.chen@momenta.ai>
2025-03-27 14:42:00 +0800

    submit sast framework 0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,65 @@
-#include "clang/AST/RecursiveASTVisitor.h"
-#include <iostream>
+#include <clang/AST/ASTContext.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+using namespace clang;
+using namespace clang::tooling;
+
+int llvm::DisableABIBreakingChecks = 1;
+
+class FunctionVisitor : public RecursiveASTVisitor<FunctionVisitor> {
+public:
+  explicit FunctionVisitor(ASTContext *Context) : Context(Context) {}
+
+  bool VisitFunctionDecl(FunctionDecl *Declaration) {
+    if (Declaration->isThisDeclarationADefinition()) {
+      llvm::outs() << "Found function: "
+                   << Declaration->getNameInfo().getName().getAsString()
+                   << "\n";
+    }
+    return true;
+  }
+
+private:
+  ASTContext *Context;
+};
+
+class FunctionASTConsumer : public ASTConsumer {
+public:
+  explicit FunctionASTConsumer(ASTContext *Context) : Visitor(Context) {}
+
+  virtual void HandleTranslationUnit(ASTContext &Context) override {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+
+private:
+  FunctionVisitor Visitor;
+};
+
+class FunctionFrontendAction : public ASTFrontendAction {
+public:
+  virtual std::unique_ptr<ASTConsumer>
+  CreateASTConsumer(CompilerInstance &CI, StringRef file) override {
+    return std::make_unique<FunctionASTConsumer>(&CI.getASTContext());
+  }
+};
+
+// Command line options
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+
+int main(int argc, const char **argv) {
+  auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
+  if (!ExpectedParser) {
+    llvm::errs() << ExpectedParser.takeError();
+    return 1;
+  }
+  CommonOptionsParser &OptionsParser = *ExpectedParser;
+
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+
+  return Tool.run(newFrontendActionFactory<FunctionFrontendAction>().get());
 }
