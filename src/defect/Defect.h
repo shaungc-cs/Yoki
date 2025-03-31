a408cfcbcd9640ba7fa299b51342d06b3e2a4e18
Shuang Chen <shuang.chen@momenta.ai>
2025-03-31 10:48:54 +0800

    update: start to code defect related part

--- src/defect/Defect.h
+++ src/defect/Defect.h
@@ -1,4 +1,15 @@
 #ifndef DD43D779_114E_4540_9E76_5E38ED4204A3
 #define DD43D779_114E_4540_9E76_5E38ED4204A3
+#include "CheckerBase.h"
+#include "clang/Tooling/Tooling.h"
+#include <string>
 
+class Defect {
+
+private:
+  std::string defectId;
+  CheckerBase *belongingChecker;
+  std::string defectMessage;
+  clang::SourceRange sourceRange;
+};
 #endif /* DD43D779_114E_4540_9E76_5E38ED4204A3 */
