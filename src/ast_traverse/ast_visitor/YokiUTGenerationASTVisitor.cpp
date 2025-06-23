595e8e1577faab3e8f809293a0986c24990c4f6d
cratelschen <cratels@163.com>
2025-06-23 21:11:20 +0800

    add class FunctionDeclNode

--- src/ast_traverse/ast_visitor/YokiUTGenerationASTVisitor.cpp
+++ src/ast_traverse/ast_visitor/YokiUTGenerationASTVisitor.cpp
@@ -3,18 +3,93 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 
-bool YokiUTGenerationASTVisitor::VisitFunctionDecl(FunctionDecl *node) {
-  spdlog::info("Visiting function declaration: {}", node->getNameAsString());
-  // 只收集定义了的函数（有函数体的函数）
-  if (!node->hasBody()) {
-    spdlog::warn("Function {} does not have a body, skipping.",
-                 node->getNameAsString());
-    return true; // 返回true表示继续遍历AST
-  }
+#include "clang/Basic/SourceManager.h"
 
-  auto functionDeclNode = std::make_shared<FunctionDeclNode>(
-      node, context); // 创建FunctionDeclNode实例
-  YokiConfig::getInstance().addFunctionDeclNode(functionDeclNode);
-  spdlog::info("Collected function declaration: {}", node->getNameAsString());
+#include "llvm/Support/raw_ostream.h"
+#include <fstream>
+#include <string>
+
+bool YokiUTGenerationASTVisitor::VisitFunctionDecl(FunctionDecl *node) {
+  // spdlog::info("Visiting function declaration: {}", node->getNameAsString());
+  // // 只收集定义了的函数（有函数体的函数）
+  // if (!node->hasBody()) {
+  //   spdlog::warn("Function {} does not have a body, skipping.",
+  //                node->getNameAsString());
+  //   return true; // 返回true表示继续遍历AST
+  // }
+
+  // auto functionDeclNode = std::make_shared<FunctionDeclNode>(
+  //     node, Context); // 创建FunctionDeclNode实例
+  // YokiConfig::getInstance().addFunctionDeclNode(functionDeclNode);
+  // spdlog::info("Collected function declaration: {}",
+  // node->getNameAsString());
+  //     OS << "===== Function Declaration at ";
+
+  std::error_code EC;
+  std::unique_ptr<llvm::raw_fd_ostream> OSP =
+      std::make_unique<llvm::raw_fd_ostream>("./OutputFilename.txt", EC);
+  // 打印位置信息
+  auto &OS = *OSP;
+  FullSourceLoc FullLocation = Context->getFullLoc(node->getBeginLoc());
+  if (FullLocation.isValid()) {
+    OS << FullLocation.getSpellingLineNumber() << ":"
+       << FullLocation.getSpellingColumnNumber() << " in "
+       << FullLocation.getManager().getFilename(FullLocation).str();
+  }
+  OS << " =====\n\n";
+
+  // if (UsePrettyPrint) {
+  //   // 方式1: 使用自定义格式化输出
+  //   OS << "// Signature:\n";
+  //   std::string TypeStr = node->getReturnType().getAsString(Policy) + " ";
+  //   TypeStr += node->getNameAsString();
+  //   TypeStr += "(";
+
+  //   for (unsigned i = 0, e = node->getNumParams(); i != e; ++i) {
+  //     if (i)
+  //       TypeStr += ", ";
+  //     TypeStr += node->getParamDecl(i)->getType().getAsString(Policy);
+  //     if (node->getParamDecl(i)->getName().size())
+  //       TypeStr += " " + node->getParamDecl(i)->getName().str();
+  //   }
+
+  //   TypeStr += ")";
+  //   OS << TypeStr << "\n\n";
+
+  //   // 打印限定符和存储类
+  //   if (node->isInlined())
+  //     OS << "inline ";
+  //   if (node->isVirtualAsWritten())
+  //     OS << "virtual ";
+  //   if (node->isStatic())
+  //     OS << "static ";
+  //   if (node->isExternC())
+  //     OS << "extern \"C\" ";
+
+  //   // 打印函数体（如果有）
+  //   if (node->hasBody()) {
+  //     OS << "// Has body: Yes\n";
+  //     // 获取函数体的源代码
+  //     SourceRange Range = node->getBody()->getSourceRange();
+  //     if (Range.isValid()) {
+  //       SourceManager &SM = Context->getSourceManager();
+  //       OS << "// Body:\n";
+  //       OS << Lexer::getSourceText(CharSourceRange::getTokenRange(Range), SM,
+  //                                  Context->getLangOpts())
+  //                 .str()
+  //          << "\n";
+  //     }
+  //   } else {
+  //     OS << "// Has body: No\n";
+  //   }
+
+  // } else {
+  // 方式2: 使用内置的dump方法
+  OS << "// Raw AST Dump:\n";
+  node->dump(OS);
+  OS << "\n";
+  // }
+
+  OS << "\n==========================================================\n\n";
   return true; // 返回true表示继续遍历AST
 }
--- src/ast_traverse/ast_visitor/YokiUTGenerationASTVisitor.h
+++ src/ast_traverse/ast_visitor/YokiUTGenerationASTVisitor.h
@@ -2,6 +2,7 @@
 #define B9A6BE48_F3D8_4505_810D_31AD3F9EE71B
 
 #include <clang/AST/AST.h>
+#include <clang/AST/PrettyPrinter.h>
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <spdlog/spdlog.h>
 
@@ -10,12 +11,18 @@ using namespace clang;
 class YokiUTGenerationASTVisitor
     : public RecursiveASTVisitor<YokiUTGenerationASTVisitor> {
 public:
-  explicit YokiUTGenerationASTVisitor(ASTContext *context) : context(context) {}
+  explicit YokiUTGenerationASTVisitor(ASTContext *context)
+      : Context(context), Policy(Context->getPrintingPolicy()) {
+    // 设置打印策略以获得更详细的输出
+    Policy.PrintCanonicalTypes = true;
+    Policy.FullyQualifiedName = true;
+  }
 
   bool VisitFunctionDecl(FunctionDecl *node);
 
 private:
-  ASTContext *context;
+  ASTContext *Context;
+  PrintingPolicy Policy;
 };
 
 #endif /* B9A6BE48_F3D8_4505_810D_31AD3F9EE71B */
